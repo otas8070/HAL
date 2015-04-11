d27c60e392f845b9d98ea07e8badc308f5966dd3
佐藤翔野 <shoya@dhcp231.ht.sfc.keio.ac.jp>
2015-04-11 15:11:54 +0900

    CardControllerをC#で追加。↑キーでDrawCardを調節

---
+++ Assets/Sorce/CardController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+ [RequireComponent (typeof(Animator))]
+public class CardController : MonoBehaviour {
+ 
+	private Animator animator;
+	private int DrawCardId;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		DrawCardId = Animator.StringToHash ("DrawCard");
+		Debug.Log (DrawCardId);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			animator.SetBool(DrawCardId, true);
+		}else{
+			animator.SetBool(DrawCardId, false);
+		}
+	
+	}
+}
