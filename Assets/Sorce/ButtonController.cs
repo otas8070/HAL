3254368093cf68df8f1400cccf9ca92ce3559b8f
佐藤翔野 <shoya@dhcp231.ht.sfc.keio.ac.jp>
2015-04-11 17:11:38 +0900

    ボタンとカードのスクリプトを導入

---
+++ Assets/Sorce/ButtonController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+[RequireComponent (typeof(Animator))]
+public class ButtonController : MonoBehaviour {
+
+	private CardController CardControl;
+
+	void Start () {
+		GameObject go = GameObject.Find ("101");
+		CardControl = go.GetComponent<CardController>();
+		CardControl.init ();
+	}
+
+	public void ClickHi (){
+		CardControl.DeleteCard (true);
+	}
+		
+}
--- Assets/Sorce/CardController.cs
+++ Assets/Sorce/CardController.cs
@@ -3,43 +3,39 @@ using System.Collections;
 
  [RequireComponent (typeof(Animator))]
 public class CardController : MonoBehaviour {
- 
+
+	//private GameObject CardAnimate;
 	private Animator animator;
 	private int DrawCardId;
 	private int DeleteCardId;
 
-	// Use this for initialization
-	void Start () {
-		animator = GetComponent<Animator> (); //アニメータを取得
+	public void init () {
+		//CardAnimate = GameObject.Find ("101");
+		animator = GetComponent<Animator> ();//アニメータを取得
 		//各パラメーターのIDを取得
 		DrawCardId = Animator.StringToHash ("DrawCard"); 
 		DeleteCardId = Animator.StringToHash ("DeleteCard");
+		//最初に１回引く
+		DrawCard (true);
+	}
+		
+	void initDeleteCard(){
+		DeleteCard (false);
 	}
 	 
-	// Update is called once per frame
-	void Update () {
-		//↑キーで制御
-		if (Input.GetKey(KeyCode.UpArrow)) {
-			DrawCard (true);
-			DeleteCard (true);
-		}else{
-			DrawCard (false);
-			DeleteCard (false);
-		}
-	} 
-
 	//DrawCard制御関数
-	void DrawCard (bool chk){
+	public void DrawCard (bool chk){
 		if (chk)
 			animator.SetBool (DrawCardId, true);
 		else
 			animator.SetBool (DrawCardId, false);
 	}
 
-	void DeleteCard (bool chk){
+	public void DeleteCard (bool chk){
 		if (chk)
 			animator.SetBool (DeleteCardId, true);
 		else
 			animator.SetBool (DeleteCardId, false);
 	}
+		
 }
